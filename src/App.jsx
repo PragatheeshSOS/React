2103e22116d458fa1a0397bc2e4d43f2a8ab9e0c
PragatheeshSOS <pragatheeshs66@gmail.com>
2024-09-16 14:47:49 +0530

    Color Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ const App = () => {
         <div className="h-screen flex justify-center items-center bg-black">
             <div className="h-3/6 w-[30%] flex justify-center items-center bg-[#9a091f] rounded-md shadow-2xl">
             <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
-                <input type="text" name="" id="" placeholder="Name" className="p-3 bg-black w-full font-bold outline-none active: outline-none focus: border-b-2 hover:border-red-300"/>
+                <input type="text" name="" id="" placeholder="Enter Your Name" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
+                <input type="email" name="" id="" placeholder="Enter Your Email" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
             </form>
             </div>
         </div>
