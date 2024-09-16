fcc57b087b1a8f789af939d3d93f4416b0ca44b8
PragatheeshSOS <pragatheeshs66@gmail.com>
2024-09-16 14:01:56 +0530

    Loaded

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+    return(
+        <>
+        <div className="h-screen flex justify-center items-center bg-black">
+            <div className="h-3/6 w-[30%] flex justify-center items-center bg-[#9a091f] rounded-md shadow-2xl">
+            <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
+                <input type="text" name="" id="" placeholder="Name" className="p-3 bg-black w-full font-bold outline-none active: outline-none focus: border-b-2 hover:border-red-300"/>
+            </form>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default App;
