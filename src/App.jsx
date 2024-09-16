5a227d11843a587bc5c5a5617602c87de31b9ae7
PragatheeshSOS <pragatheeshs66@gmail.com>
2024-09-16 14:51:39 +0530

    Extra Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
             <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
                 <input type="text" name="" id="" placeholder="Enter Your Name" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
                 <input type="email" name="" id="" placeholder="Enter Your Email" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
+                <input type="number" name="" id="" placeholder="Enter Your Number" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
+                <input type="password" name="" id="" placeholder="Enter Your Password" className="p-3 bg-black w-full text-[#d7354d] font-bold outline-none active: outline-none focus: border-b-4 hover:border-red-600"/>
             </form>
             </div>
         </div>
