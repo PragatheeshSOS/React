04ccb4bc30b0bbbc56a7f9c4c7794ca6d66722e1
PragatheeshSOS <pragatheeshs66@gmail.com>
2024-09-17 11:41:01 +0530

    MODY

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 const NavBar = ({data})=> {
     console.log(data)
     return (
-        <div className="w-full h-[3vh] grad-blue-bg text-white flex justify-center">
-            {data.Appname}
+        <div className="w-full h-[3vh] grad-blue-bg text-white flex justify-center items-center gap-4">
+            {data.Appname} {data.Appdata}
         </div>
     )
 }
